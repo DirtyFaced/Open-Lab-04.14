7f42b80d38adcb08b2237c9e8f9c2a7960691d38
Jakub Dipčár <jakub.dipcar@gmail.com>
2020-11-16 10:56:59 +0100

    najs

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -6,7 +6,35 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            int maxdlzka = 0;
+            int nowdlzka = 0;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length > maxdlzka)
+                {
+                    maxdlzka = strings[i].Length;
+                }
+            }
+            for (int ii = 0; ii < maxdlzka + 3; ii++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("*");
+            for (int iii = 0; iii < strings.Length; iii++)
+            {
+                nowdlzka = maxdlzka - strings[iii].Length;
+                Console.Write("* " + strings[iii]);
+                for (int iiii = 0; iiii < nowdlzka; iiii++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" *");
+            }
+            for (int ii = 0; ii < maxdlzka + 3; ii++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("*");
         }
     }
 }
